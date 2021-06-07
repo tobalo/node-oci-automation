cb321fafeacc2d453f1002c0be41909e8be6e36a
Tobalo <nexxis817@gmail.com>
2021-06-07 13:54:15 -0500

    Basic node.js and OCI OS init

---
+++ index.js
@@ -0,0 +1,52 @@
+const oci = require('oci-sdk');
+const ociCommon = require('oci-common');
+require('dotenv').config();
+
+
+const ociAuth = new ociCommon.ConfigFileAuthenticationDetailsProvider();
+
+const osClient = new oci.objectstorage.ObjectStorageClient(ociAuth);
+
+
+(async () => {
+    try {
+        console.log("Getting OCI OS Namespace...");
+        const request = {};
+        const nsResponse = await ociClient.getNamespace(request);
+        const namespace = nsResponse.value;
+        console.log('OCI OS Namespace:', namespace);
+
+        const bucketName = 'oci_bucket';
+
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        today = yyyy + '-' + mm + '-' + '06';
+        console.log('Date time:', today);
+
+        const objectName = 'OBJECT_NAME';
+        console.log('Object Name:', objectName);
+
+        const getObjectRequest = {
+            objectName: objectName,
+            bucketName: bucketName,
+            namespaceName: namespace,
+            httpResponseContentEncoding: null
+        };
+
+        const getObjectResponse = await ociClient.getObject(getObjectRequest);
+        console.log("Get Object executed...");
+        console.log("Object Data:", getObjectResponse);
+
+        console.log("Swifting through getObjectResponse...");
+        console.log(getObjectResponse);
+
+        // TODO: Parse Object Storage Response for signals
+        console.log("Deserializing Object Response...");
+        console.log(getObjectResponse.value);
+
+    } catch (e) {
+        console.log("Error:", e);
+    }
+})();
